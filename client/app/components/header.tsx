9c5f88716e95ee817c3fe8210df9b87d36f27ffb
Nathan Santiago <santiagonathan9254@gmail.com>
2024-09-25 17:38:28 -0400

    Create user log in and register. Needs work.

--- client/app/components/header.tsx
+++ client/app/components/header.tsx
@@ -1,11 +1,26 @@
+"use client"
+
 import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useRouter } from 'next/navigation';
+import { supabase } from '../../lib/supabase';
 
 const Header: React.FC = () => {
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) console.error('Error logging out:', error.message);
+        else {
+            console.log('User logged out');
+            router.replace('/');
+        }
+    };
+
     return (
         <header className='flex justify-between'>
             <h1 className='title'>RamEats</h1>
-            <Avatar>
+            <Avatar onClick={handleLogout}>
                 <AvatarImage src="https://github.com/shadcn.png" />
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import RamEatsLogo from "../public/RamEatsLogo (1).png";
 import { Button } from "@/components/ui/button"
@@ -12,8 +14,51 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { supabase } from "@/lib/supabase";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+      if (session) {
+        router.push("/dashboard/home");
+      }
+    };
+    if (typeof window !== 'undefined') {
+      checkSession();
+    }
+  }, []);
+
+  async function handleLogin() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("Logged " + data.user + " in successfully!");
+    }
+  }
+
+  async function handleRegister() {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("Registered " + data.user + " successfully!");
+    }
+  }
+
   return (
     <main className="min-h-screen flex justify-center items-center flex-col">
       <Image
@@ -38,12 +83,14 @@ export default function Home() {
           <div className="grid gap-4 py-4">
             <div className="grid grid-cols-4 items-center gap-4">
               <Label htmlFor="username" className="text-right">
-                Username
+                Email
               </Label>
               <Input
-                id="username"
-                defaultValue="nathansantiag0"
+                id="email"
+                type="email"
                 className="col-span-3"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
@@ -52,13 +99,58 @@ export default function Home() {
               </Label>
               <Input
                 id="password"
-                defaultValue="********"
+                type="password"
+                value={password}
                 className="col-span-3"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
           <DialogFooter>
-            <Button type="submit">Login</Button>
+            <Button type="submit" onClick={handleLogin}>Login</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Register</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Register</DialogTitle>
+            <DialogDescription>
+              Enter your username and password to register your account.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="email" className="text-right">
+                Email
+              </Label>
+              <Input
+                id="email"
+                type="email"
+                className="col-span-3"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="password" className="text-right">
+                Password
+              </Label>
+              <Input
+                id="password"
+                type="password"
+                className="col-span-3"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit" onClick={handleRegister}>Register</Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
